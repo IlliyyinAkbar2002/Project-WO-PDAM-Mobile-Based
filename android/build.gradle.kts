549392f0d88cc4e77a4fdda29fd9e5954517aa0c
Illiyyin Akbar A <illiyyin.akbar123@gmail.com>
2026-05-03 19:45:16 +0700

    Update dependencies and enhance Android build configuration
    - Added new dependencies in `pubspec.yaml` for `flutter_to_pdf` and other packages including `archive`, `barcode`, `bidi`, `image`, `pdf`, `posix`, and `qr` in `pubspec.lock`.
    - Enhanced Android build configuration in `build.gradle.kts` to set Java compatibility to version 11 for all subprojects.
    - Updated `gradle.properties` to disable Kotlin incremental compilation for improved build stability.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,25 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
+                }
+            }
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
